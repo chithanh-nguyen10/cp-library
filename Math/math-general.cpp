83c8d86df9c50e16af28bcae3ed0c7dc2ce1c950
unknown <tenchithanh@gmail.com>
2026-02-25 13:46:19 +0700

    Move combin to a separate file

--- Math/math-general.cpp
+++ Math/math-general.cpp
@@ -1,96 +1,63 @@
-namespace math {
-    template<int _MOD>
-    struct Modint{
-        static const int mod = _MOD;
-        int v;
+template<int _MOD>
+struct Modint{
+    static const int mod = _MOD;
+    int v;
 
-        Modint() : v(0) {}
-        Modint(int _v) : v(_v % mod) {
-            if (v < 0) v += mod;
-        }
-        Modint& operator += (const Modint &other) {
-            v += other.v;
-            if (v >= mod) v -= mod;
-            return *this;
-        }
+    Modint() : v(0) {}
+    Modint(int _v) : v(_v % mod) {
+        if (v < 0) v += mod;
+    }
+    Modint& operator += (const Modint &other) {
+        v += other.v;
+        if (v >= mod) v -= mod;
+        return *this;
+    }
 
-        Modint& operator -= (const Modint &other) {
-            v -= other.v;
-            if (v < 0) v += mod;
-            return *this;
-        }
+    Modint& operator -= (const Modint &other) {
+        v -= other.v;
+        if (v < 0) v += mod;
+        return *this;
+    }
 
-        Modint& operator *= (const Modint &other) {
-            v = 1ll * v * other.v % mod;
-            return *this;
-        }
+    Modint& operator *= (const Modint &other) {
+        v = 1ll * v * other.v % mod;
+        return *this;
+    }
 
-        Modint pow(long long k) const {
-            Modint res(1), base = *this;
-            while (k) {
-                if (k & 1) res *= base;
-                base *= base;
-                k >>= 1;
-            }
-
-            return res;
+    Modint pow(long long k) const {
+        Modint res(1), base = *this;
+        while (k) {
+            if (k & 1) res *= base;
+            base *= base;
+            k >>= 1;
         }
 
-        Modint inv() const {return pow(mod - 2);}
+        return res;
+    }
 
-        Modint& operator /= (const Modint &other) {
-            *this *= other.inv();
-            return *this;
-        }
+    Modint inv() const {return pow(mod - 2);}
 
-        friend Modint operator + (Modint a, const Modint &b) {return a += b;}
-        friend Modint operator - (Modint a, const Modint &b) {return a -= b;}
-        friend Modint operator * (Modint a, const Modint &b) {return a *= b;}
-        friend Modint operator / (Modint a, const Modint &b) {return a /= b;}
+    Modint& operator /= (const Modint &other) {
+        *this *= other.inv();
+        return *this;
+    }
 
-        Modint operator - () const {return Modint(0) - *this;}
+    friend Modint operator + (Modint a, const Modint &b) {return a += b;}
+    friend Modint operator - (Modint a, const Modint &b) {return a -= b;}
+    friend Modint operator * (Modint a, const Modint &b) {return a *= b;}
+    friend Modint operator / (Modint a, const Modint &b) {return a /= b;}
 
-        bool operator == (const Modint &t) const {return v == t.v;}
-        bool operator != (const Modint &t) const {return v != t.v;}
-        bool operator < (const Modint &t) const {return v < t.v;}
+    Modint operator - () const {return Modint(0) - *this;}
 
-        friend istream& operator >> (istream &stream, Modint &other) {
-            return stream >> other.v;
-        } 
+    bool operator == (const Modint &t) const {return v == t.v;}
+    bool operator != (const Modint &t) const {return v != t.v;}
+    bool operator < (const Modint &t) const {return v < t.v;}
 
-        friend ostream& operator << (ostream &stream, const Modint &other) {
-            return stream << other.v;
-        }
-    };
+    friend istream& operator >> (istream &stream, Modint &other) {
+        return stream >> other.v;
+    } 
 
-    template <int _MOD>
-    struct Combinatorics{
-        int n;
-        vector<Modint<_MOD>> fact, ifact;
-
-        Combinatorics(int _n) : n(_n) {
-            fact.resize(n + 1, 0);
-            ifact.resize(n + 1, 0);
-
-            fact[0] = 1;
-            for (int i = 1; i <= n; ++i) fact[i] = fact[i - 1] * i;
-            ifact[n] = fact[n].inv();
-            for (int i = n - 1; i >= 0; --i) ifact[i] = ifact[i + 1] * (i + 1);
-        }
-
-        Modint<_MOD> C(int n, int k) {
-            if (k < 0 || k > n) return 0;
-            return fact[n] * ifact[k] * ifact[n - k];
-        }
-
-        /*
-            Count the number of non-negative integer solutions of:
-            x_1 + x_2 + ... + x_n = m
-
-            Tested: https://cses.fi/problemset/task/1717
-        */
-        Modint<_MOD> euler(int n, int m) {
-            return C(n + m - 1, m);
-        }
-    };
-}
+    friend ostream& operator << (ostream &stream, const Modint &other) {
+        return stream << other.v;
+    }
+};
