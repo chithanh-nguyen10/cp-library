b55b6006d34361da9dfc50c5b9cfc49c93782e00
unknown <tenchithanh@gmail.com>
2026-02-28 18:11:58 +0700

    Change BIT's implementation style

--- DataStructure/fenwicktree.cpp
+++ DataStructure/fenwicktree.cpp
@@ -1,45 +1,47 @@
 /*
 Name: Fenwick Tree
 Tested:
-- CSES 1648: https://cses.fi/problemset/task/1648
-- CSES 2169: https://cses.fi/problemset/task/2169
-- CSES 1748: https://cses.fi/problemset/task/1748
+- https://cses.fi/problemset/task/1648
+- https://cses.fi/problemset/task/1651
+- https://oj.vnoi.info/problem/orderset (kth)
 */
 
 // Indexed from 1
-// Supports Point Update Range Sum
-struct FenwickTree{
-    int n, lg;
-    vector<int> fen;
- 
-    FenwickTree() {
-    }
- 
-    FenwickTree(int _n) : n(_n) {
-        lg = __lg(n);
-        fen.resize(n + 5, 0);
-    }
- 
-    void update(int idx, int val) {
-        for (int i = idx; i <= n; i += i & -i)
-            fen[i] += val;
+// Supports Point Update, Range Sum, Kth
+// Complexity: O(log n)
+template <class T>
+struct FenwickTree {
+    int n;
+    vector<T> fen;
+
+    FenwickTree(int _n = 0) {
+        init(_n);
     }
 
-    void updateRange(int l, int r, int val) {
-        update(l, val);
-        update(r + 1, -val);
+    void init(int _n) {
+        n = _n;
+        fen.resize(n + 5, T());
     }
- 
-    int get(int idx) {
-        int sum = 0;
+
+    void update(int idx, T v) {
+        for (int i = idx; i <= n; i += i & -i)
+            fen[i] += v;
+    }
+
+    T get(int idx) {
+        T sum = T();
         for (int i = idx; i; i -= i & -i)
             sum += fen[i];
         return sum;
     }
 
-    int lower_bound(int x) {
-        int pos = 0, sum = 0;
-        for (int i = lg; i >= 0; --i) {
+    T query(int l, int r) {
+        return get(r) - get(l - 1);
+    }
+
+    int kth(T x) {
+        int pos = 0; T sum = T();
+        for (int i = __lg(n); i >= 0; --i) {
             if ((pos | (1 << i)) <= n && sum + fen[pos | (1 << i)] < x) {
                 pos |= (1 << i);
                 sum += fen[pos];
@@ -47,9 +49,5 @@ struct FenwickTree{
         }
 
         return pos + 1;
-    }
- 
-    int query(int l, int r) {
-        return get(r) - get(l - 1);
     }
 };
